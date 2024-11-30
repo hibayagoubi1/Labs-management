90b3b179a703e459b6979d4def1c3a5a4982d160
hibayagoubi1 <hibayaagoubi4@gmail.com>
2024-11-30 13:38:35 +0100

    change

--- identity-service/src/test/java/com/javatechie/IdentityServiceApplicationTests.java
+++ identity-service/src/test/java/com/javatechie/IdentityServiceApplicationTests.java
@@ -2,12 +2,14 @@ package com.javatechie;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test") // Utilise le fichier application-test.properties
 @SpringBootTest
-class IdentityServiceApplicationTests {
+public class IdentityServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// Votre logique de test ici
 	}
-
 }
