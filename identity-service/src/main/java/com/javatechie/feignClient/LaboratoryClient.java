f4ae9c368eea268803b00829e87f776f78fd84f1
hibayagoubi1 <hibayaagoubi4@gmail.com>
2024-12-17 23:34:59 +0100

    change

--- identity-service/src/main/java/com/javatechie/feignClient/LaboratoryClient.java
+++ identity-service/src/main/java/com/javatechie/feignClient/LaboratoryClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "laboratory-service", url = "http://localhost:8086")
+@FeignClient(name = "labs-service", url = "${laboratory.service.url}")
 public interface LaboratoryClient {
     @GetMapping("/labs/{id}")
     Laboratory getLaboratoryById(@PathVariable("id") Long id);
